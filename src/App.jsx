b56f1a7df4f1c0bdc2eafa1457caa77985ab85f1
Kate <mitflash56@gmail.com>
2023-08-02 16:24:33 +0300

    themes added
    Added themes and saving them in localStorage, fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,214 @@
-import './styles/input.css'
-import {TodoList, Todo}  from './todolist'
-import { useState } from 'react';
+import './input.css'
+import { useState, useEffect } from 'react';
 
+
+function Header({theme, setTheme}){
+  return(
+    <div className="header-container">
+      <h1 className='header-h1'>TODO App</h1>
+      <button className={`theme-btn ${theme==='day'? 'day-bg' : 'night-bg'}`} onClick={() => theme==='day'? setTheme('night') : setTheme('day')}></button>
+    </div>
+  )
+}
+function TodoList({ todos, setTodos}){
+  let todoList = todos.map(todo => {
+    return (
+      <Todo 
+        key={todo.note}
+        todo={todo}
+        setTodos={setTodos} 
+        todos={todos}
+      />
+    );
+  })
+  return(
+    <ul className='todo-list'>{todoList}</ul>
+  )
+}
+function Todo({todo, setTodos, todos}) {
+  
+  const onChangeCheckBox = () => {
+    const updatedTodos = todos.map(t => {
+      if (t.note === todo.note) {
+        return {
+          ...t,
+          isChecked: !t.isChecked  
+        };
+      }
+      return t;
+    });
+  
+    setTodos(updatedTodos);
+  }
+  return (
+    <div className="custom-checkbox todo">
+      <input
+        type="checkbox"
+        onChange={onChangeCheckBox}
+        defaultValue={todo.isChecked}
+      />
+      <div className="svg-wrapper">
+      <svg viewBox="0 0 60 40" aria-hidden="true" focusable="false"className='checkbox-svg'>
+      <path
+      className={todo.isChecked ? 'checkbox-checked' : ''}
+      d="M21,2 C13.4580219,4.16027394 1.62349378,18.3117469 3,19 C9.03653312,22.0182666 25.2482171,10.3758914 30,8 C32.9363621,6.53181896 41.321398,1.67860195 39,4 C36.1186011,6.8813989 3.11316157,27.1131616 5,29 C10.3223659,34.3223659 30.6434647,19.7426141 35,18 C41.2281047,15.5087581 46.3445303,13.6554697 46,14 C42.8258073,17.1741927 36.9154967,19.650702 33,22 C30.3136243,23.6118254 17,31.162498 17,34 C17,40.4724865 54,12.4064021 54,17 C54,23.7416728 34,27.2286213 34,37"
+      strokeWidth={4}
+      fill="none"
+      strokeDasharray={270}
+      strokeDashoffset={270}
+      />
+    </svg>
+    </div>
+      <p className={todo.isChecked ? 'todo-note strikethrough' : 'todo-note'}>{todo.note}</p>
+    </div>
+  
+
+
+  );
+}
 function Navigation({todos, setTodos}){
-
+  const [error, setError] = useState(false);
   let [text, setText] = useState('')
-
   const handleTextChange = (e) => {
     setText(e.target.value)
   }
-
+  const showError = () => {
+    setError(true);
+    setText('This TODO is already in the list');
+    setTimeout(() => {
+      setError(false);
+      setText('');
+    }, 2000); 
+  }
   const handleAddTodo = () => {
-    const newTodos = [...todos];
-    newTodos.unshift({
-      note: text,
-      isChecked: false,
+    const duplicate = todos.some(todo => {
+      return todo.note === text; 
     });
-    setTodos(newTodos);
+  
+    if (duplicate) {
+      showError();
+      setText('');
+      return;
+    }
+  
+    const newTodo = {
+      note: text,
+      isChecked: false
+    };
+  
+    setTodos([newTodo,...todos]);
     setText('');
   }
-
   return (
-    <form onSubmit={handleAddTodo}>
+    <div className="nav-form" onKeyDown={e => {
+      if(e.key === 'Enter') {
+        handleAddTodo();}}}>
       <input 
-        type="text"
+        type="text-area"
         value={text}
         onChange={handleTextChange} 
+        className={error ? 'nav-input text-red' : 'nav-input'}
+        placeholder={error ? 'This TODO is already in the list' : ' Your TODO'}
+        autoFocus
       />
-      <button type="submit">Add Todo</button>
-    </form>
+      <button type="submit" className='nav-btn' onClick={handleAddTodo}>Add Todo</button>
+    </div>
+      
   )
 
 }
-
-function AdditionalTools(){
-
+function Tip({todos}) {
   return (
-    <button>Delete all checked</button>
-  )
-
+    <p style={{display: todos.length === 0 ? 'block' : 'none'}} className='tip'>
+      Your todos shall be here
+    </p>
+  );
 }
+function AdditionalTools({todos, setTodos}) {
 
+  const isEverythingChecked = todos.every(todo => todo.isChecked)
+
+  const handleToggleAll = () => {
+    let newTodos = todos.map(todo => ({
+      note: todo.note,
+      isChecked: true
+    }));
+    
+    setTodos(newTodos);
+  }
+  
+  const handleUntoggleAll = () => {
+    let newTodos = todos.map(todo => {  
+      return {
+        ...todo,
+        isChecked: false
+      }
+    });
+  
+    setTodos(newTodos);
+  }
+  const handleDeleteChecked = () => {
+    let newTodos = todos.filter(todo => !todo.isChecked)
+    newTodos = newTodos.map(todo => {  
+      return {
+        ...todo,
+        isChecked: false
+      }
+    });
+    setTodos(newTodos)}
+  return (
+    <div className={`additional-tools ${todos.length > 0 ? '' : 'opacity-zero'}`}>
+      <button onClick={handleDeleteChecked} className='aditional-btn delete-btn'>Delete all checked</button>
+      <button 
+        onClick={isEverythingChecked ? handleUntoggleAll : handleToggleAll}
+        className={isEverythingChecked ? 'aditional-btn check-btn check-btn-green' : 'aditional-btn check-btn check-btn-red'}>
+        {isEverythingChecked ? 'Uncheck All' : 'Check All'}  
+      </button>
+      
+    </div>
+  )
+  }
 
 export default function App() {
+  const [theme, setTheme] = useState('')
+  const [todos, setTodos] = useState([])
+  // Раздел с хуками
+  useEffect(() => {
 
-  const [todos, setTodos] = useState([
-    {note:"do my homework", isChecked:false},
-    {note:"write an essay", isChecked:false},
-    {note:"plant a tree", isChecked:false},
-    {note:"grow a son", isChecked:true},
-    {note:"build a house", isChecked:false},
-    {note:"lorem ipsum", isChecked:true},
-  ])
+    const storedTodos = JSON.parse(localStorage.getItem('todos'));
+    
+    if (storedTodos) {
+        setTodos(prev => [...prev, ...storedTodos])   
+      }
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+    
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);    
+  }, [theme]);
+
+  useEffect(() => {
+    document.body.className = theme === 'day' ? 'day-theme' : 'night-theme';
+  }, [theme]);
+  
   return (
-    <div className="todo-app">
-
-      <Navigation todos={todos} setTodos={setTodos} />
-
-      <TodoList 
-  todoList={todos.map((todo, index) => {
-
-    const numberInArray = index + 1;
-
-    return (
-      <Todo 
-        key={numberInArray} 
-        todo={todo}
-      />
-    );
-
-  })}/>
-
-      <AdditionalTools />
-
+    <div className={`todo-app ${theme==='day' ? 'day-theme' : 'night-theme'}`}>
+      <Header theme={theme} setTheme={setTheme}/>
+      <Navigation className='navigation' todos={todos} setTodos={setTodos} />
+      <TodoList  todos={todos} setTodos={setTodos}/>
+      <Tip todos={todos}/>
+      <AdditionalTools className='aditional-tools' todos={todos} setTodos={setTodos}/>
     </div>
   )
 
